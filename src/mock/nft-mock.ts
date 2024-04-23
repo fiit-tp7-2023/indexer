d853bd99b919d0403184abf65a8fb0aa5516956b
Adam Prisenznak <adam.prisen@gmail.com>
2024-04-23 11:21:36 +0200

    :clown_face: more mocked NFTs

--- src/mock/nft-mock.ts
+++ src/mock/nft-mock.ts
@@ -69,6 +69,156 @@ export const mockNfts: NftEntity[] = [
     createdAtBlock: 0,
   }),
   new NftEntity({
+    id: '0x0000000000000000000000000000000000000001_ETH_3',
+    tokenId: BigInt(3),
+    name: 'Azuki #3138 MOCK',
+    image: 'ipfs://QmYDvPAXtiJg7s8JdRBSLWdgSphQdac8j1YuQNNxcGE1hg/3138.png',
+    attributes: [
+      {
+        trait_type: 'Type',
+        value: 'Human',
+      },
+      {
+        trait_type: 'Hair',
+        value: 'Powder Blue Disheveled',
+      },
+      {
+        trait_type: 'Clothing',
+        value: 'Light Kimono',
+      },
+      {
+        trait_type: 'Eyes',
+        value: 'White',
+      },
+      {
+        trait_type: 'Mouth',
+        value: 'Relaxed',
+      },
+      {
+        trait_type: 'Offhand',
+        value: 'Bean Juice',
+      },
+      {
+        trait_type: 'Background',
+        value: 'Off White D',
+      },
+    ],
+    uri: '',
+    nftCollection: mockNftCollections[0],
+    createdAtBlock: 0,
+  }),
+  new NftEntity({
+    id: '0x0000000000000000000000000000000000000001_ETH_4',
+    tokenId: BigInt(4),
+    name: 'Azuki #3500 MOCK',
+    image: 'ipfs://QmYDvPAXtiJg7s8JdRBSLWdgSphQdac8j1YuQNNxcGE1hg/3500.png',
+    attributes: [
+      {
+        trait_type: 'Type',
+        value: 'Human',
+      },
+      {
+        trait_type: 'Hair',
+        value: 'Buzzcut',
+      },
+      {
+        trait_type: 'Headgear',
+        value: 'Frog Baseball Cap',
+      },
+      {
+        trait_type: 'Ear',
+        value: 'Ear Cuffs',
+      },
+      {
+        trait_type: 'Face',
+        value: 'Reading Glasses',
+      },
+      {
+        trait_type: 'Clothing',
+        value: 'Kimono with Jacket',
+      },
+      {
+        trait_type: 'Eyes',
+        value: 'Closed',
+      },
+      {
+        trait_type: 'Mouth',
+        value: 'Sleep Bubble',
+      },
+      {
+        trait_type: 'Background',
+        value: 'Dark Blue',
+      },
+    ],
+    uri: '',
+    nftCollection: mockNftCollections[0],
+    createdAtBlock: 0,
+  }),
+  new NftEntity({
+    id: '0x0000000000000000000000000000000000000001_ETH_5',
+    tokenId: BigInt(5),
+    name: 'Pudgy Penguin #151 MOCK',
+    description: 'A collection 8888 Cute Chubby Pudgy Penquins sliding around on the freezing ETH blockchain.',
+    image: 'ipfs://QmNf1UsmdGaMbpatQ6toXSkzDpizaGmC9zfunCyoz1enD5/penguin/151.png',
+    attributes: [
+      {
+        trait_type: 'Background',
+        value: 'Purple',
+      },
+      {
+        trait_type: 'Skin',
+        value: 'Light Gray',
+      },
+      {
+        trait_type: 'Body',
+        value: 'Apron',
+      },
+      {
+        trait_type: 'Face',
+        value: 'Circle Glasses',
+      },
+      {
+        trait_type: 'Head',
+        value: 'Afro With Pick',
+      },
+    ],
+    uri: '',
+    nftCollection: mockNftCollections[0],
+    createdAtBlock: 0,
+  }),
+  new NftEntity({
+    id: '0x0000000000000000000000000000000000000001_ETH_6',
+    tokenId: BigInt(6),
+    name: 'Pudgy Penguin #200 MOCK',
+    description: 'A collection 8888 Cute Chubby Pudgy Penquins sliding around on the freezing ETH blockchain.',
+    image: 'ipfs://QmNf1UsmdGaMbpatQ6toXSkzDpizaGmC9zfunCyoz1enD5/penguin/200.png',
+    attributes: [
+      {
+        trait_type: 'Background',
+        value: 'Yellow',
+      },
+      {
+        trait_type: 'Skin',
+        value: 'Dark Gray',
+      },
+      {
+        trait_type: 'Body',
+        value: 'Crop Top',
+      },
+      {
+        trait_type: 'Face',
+        value: 'Circle Glasses',
+      },
+      {
+        trait_type: 'Head',
+        value: 'Backwards Hat Blue',
+      },
+    ],
+    uri: '',
+    nftCollection: mockNftCollections[0],
+    createdAtBlock: 0,
+  }),
+  new NftEntity({
     id: '0x0000000000000000000000000000000000000002_ETH_1',
     tokenId: BigInt(1),
     name: 'Seers Stone [PL] MOCK',
@@ -97,6 +247,109 @@ export const mockNfts: NftEntity[] = [
         key: 'Class',
         trait_type: 'Class',
         value: 'PL',
+      },
+    ],
+    uri: '',
+    nftCollection: mockNftCollections[1],
+    createdAtBlock: 0,
+  }),
+  new NftEntity({
+    id: '0x0000000000000000000000000000000000000002_ETH_2',
+    tokenId: BigInt(2),
+    name: 'Prismatic Key MOCK',
+    description: 'Change is a constant. The prismatic colors last only for a matter of moments',
+    image: 'https://nftmedia.parallelnft.com/parallel-alpha/QmPBM3hvi8dZ2CFwDSeJHTSWPkr3au7unMDSEckBaLwMHR/image.gif',
+    animationUrl:
+      'https://nftmedia.parallelnft.com/parallel-alpha/QmPBM3hvi8dZ2CFwDSeJHTSWPkr3au7unMDSEckBaLwMHR/animation.mp4',
+    externalUrl: 'https://rarible.com/token/0x76be3b62873462d2142405439777e971754e8e77:10933',
+    attributes: [
+      {
+        key: 'Rarity',
+        trait_type: 'Rarity',
+        value: 'Prime',
+      },
+      {
+        key: 'Class',
+        trait_type: 'Class',
+        value: 'Asset',
+      },
+      {
+        key: 'Parallel',
+        trait_type: 'Parallel',
+        value: 'Universal',
+      },
+      {
+        key: 'Artist',
+        trait_type: 'Artist',
+        value: 'Oscar Mar',
+      },
+    ],
+    uri: '',
+    nftCollection: mockNftCollections[1],
+    createdAtBlock: 0,
+  }),
+  new NftEntity({
+    id: '0x0000000000000000000000000000000000000002_ETH_3',
+    tokenId: BigInt(3),
+    name: 'TE4 Interceptor [SE] MOCK',
+    description:
+      'Ships come in many shapes & sizes. Interceptor-class vessels are designed as hunter-killers, far outmaneuvering larger craft.',
+    image: 'https://nftmedia.parallelnft.com/parallel-alpha/QmSJB3rokhG9fmWpiP2pV2b2hCFQP3EzbMfP6wVLgqK5PC/image.png',
+    externalUrl: 'https://rarible.com/token/0x76be3b62873462d2142405439777e971754e8e77:10567',
+    attributes: [
+      {
+        key: 'Artist',
+        trait_type: 'Artist',
+        value: 'Oscar Cafaro',
+      },
+      {
+        key: 'Parallel',
+        trait_type: 'Parallel',
+        value: 'Universal',
+      },
+      {
+        key: 'Rarity',
+        trait_type: 'Rarity',
+        value: 'Common',
+      },
+      {
+        key: 'Class',
+        trait_type: 'Class',
+        value: 'SE',
+      },
+    ],
+    uri: '',
+    nftCollection: mockNftCollections[1],
+    createdAtBlock: 0,
+  }),
+  new NftEntity({
+    id: '0x0000000000000000000000000000000000000002_ETH_4',
+    tokenId: BigInt(4),
+    name: 'Unstable Drone [SE] MOCK',
+    description:
+      'Many an enemy of Mars has managed to take down such a machine, only to learn of the immediately fatal consequences.',
+    image: 'https://nftmedia.parallelnft.com/parallel-alpha/QmNPjarRfLsNU2DS4iMme2nSfWuQKSSB6sGHeuijT6viZh/image.png',
+    externalUrl: 'https://rarible.com/token/0x76be3b62873462d2142405439777e971754e8e77:10337',
+    attributes: [
+      {
+        key: 'Artist',
+        trait_type: 'Artist',
+        value: 'Oscar Mar',
+      },
+      {
+        key: 'Parallel',
+        trait_type: 'Parallel',
+        value: 'Marcolian',
+      },
+      {
+        key: 'Rarity',
+        trait_type: 'Rarity',
+        value: 'Common',
+      },
+      {
+        key: 'Class',
+        trait_type: 'Class',
+        value: 'SE',
       },
     ],
     uri: '',
--- src/mock/transfer-mock.ts
+++ src/mock/transfer-mock.ts
@@ -13,12 +13,62 @@ export const mockNftTransfers: MockNftTransfer[] = [
   },
   {
     mockNftIndex: 2,
+    amount: BigInt('1'),
+    to: '0xFa85ef821C17BD3C4c02298DF335b8187088363F'.toLowerCase(),
+  },
+  {
+    mockNftIndex: 3,
+    amount: BigInt('1'),
+    to: '0x1804947E08e37eE0966Cc28EE169b4c6F7E42ae2'.toLowerCase(),
+  },
+  {
+    mockNftIndex: 4,
+    amount: BigInt('1'),
+    to: '0xFa85ef821C17BD3C4c02298DF335b8187088363F'.toLowerCase(),
+  },
+  {
+    mockNftIndex: 5,
+    amount: BigInt('1'),
+    to: '0x1804947E08e37eE0966Cc28EE169b4c6F7E42ae2'.toLowerCase(),
+  },
+  {
+    mockNftIndex: 6,
     amount: BigInt('10'),
     to: '0x1804947E08e37eE0966Cc28EE169b4c6F7E42ae2'.toLowerCase(),
   },
   {
-    mockNftIndex: 2,
+    mockNftIndex: 6,
     amount: BigInt('5'),
     to: '0xFa85ef821C17BD3C4c02298DF335b8187088363F'.toLowerCase(),
+  },
+  {
+    mockNftIndex: 7,
+    amount: BigInt('3'),
+    to: '0xFa85ef821C17BD3C4c02298DF335b8187088363F'.toLowerCase(),
+  },
+  {
+    mockNftIndex: 7,
+    amount: BigInt('8'),
+    to: '0x1804947E08e37eE0966Cc28EE169b4c6F7E42ae2'.toLowerCase(),
+  },
+  {
+    mockNftIndex: 8,
+    amount: BigInt('1'),
+    to: '0xFa85ef821C17BD3C4c02298DF335b8187088363F'.toLowerCase(),
+  },
+  {
+    mockNftIndex: 8,
+    amount: BigInt('5'),
+    to: '0x1804947E08e37eE0966Cc28EE169b4c6F7E42ae2'.toLowerCase(),
+  },
+  {
+    mockNftIndex: 9,
+    amount: BigInt('2'),
+    to: '0xFa85ef821C17BD3C4c02298DF335b8187088363F'.toLowerCase(),
+  },
+  {
+    mockNftIndex: 9,
+    amount: BigInt('7'),
+    to: '0x1804947E08e37eE0966Cc28EE169b4c6F7E42ae2'.toLowerCase(),
   },
 ];
